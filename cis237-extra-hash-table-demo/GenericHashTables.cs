0d3ac9eff1069518e2b4db380daebff3b12fc8c7
Michael VanderMyde <mvandermyde4231@mail.kvcc.edu>
2024-03-18 22:54:20 -0400

    Create a GenericHashTables class to practice use of hash table data structures.

---
+++ cis237-extra-hash-table-demo/GenericHashTables.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_extra_hash_table_demo
+{
+    internal class GenericHashTables<TypeKey, TypeValue>
+    {
+        private const int PRIMEVALUE = 97;
+
+        TypeValue[] theValues;
+
+        TypeKey[] theKeys;
+
+        public GenericHashTables()
+        {
+            theValues = new TypeValue[PRIMEVALUE];
+
+            theKeys = new TypeKey[PRIMEVALUE];
+
+        }
+
+        public TypeValue Get(TypeKey key)
+        {
+            int indexForValue = hashFunction(key);
+
+            return theValues[indexForValue];
+
+        }
+
+        public void Put(TypeKey key, TypeValue value)
+        {
+            int indexForValue = hashFunction(key);
+
+            theValues[indexForValue] = value;
+
+            theKeys[indexForValue] = key;
+
+        }
+
+        private int hashFunction(TypeKey key)
+        {
+            return Math.Abs(key.GetHashCode()) % PRIMEVALUE;
+
+        }
+
+        public override string ToString()
+        {
+            string returnString = "Key   => Value\n";
+
+            returnString += "------------------------\n";
+
+            for (int i = 0; i < theValues.Length; ++i)
+            {
+                if (theValues[i] != null)
+                {
+                    returnString += theKeys[i] + " => " + theValues[i] + "\n";
+
+                }
+
+            }
+
+            return returnString;
+
+        }
+
+        public string ShowArray()
+        {
+            string returnString = "Key   => Value\n";
+
+            returnString += "------------------------\n";
+
+            for (int i = 0; i < theValues.Length; ++i)
+            {
+                returnString += "[" + i + "] => " + theValues[i] + "\n";                
+
+            }
+
+            return returnString;
+
+        }
+
+    }
+
+}
--- cis237-extra-hash-table-demo/Program.cs
+++ cis237-extra-hash-table-demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace cis237_extra_hash_table_demo
 {
@@ -6,7 +7,85 @@ namespace cis237_extra_hash_table_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GenericHashTables<int, string> valleyNumToName = new GenericHashTables<int, string>();
+
+            Console.WriteLine("Adding some entries to the Hashtable");
+
+            Console.WriteLine("EX: valleyNumToName.Put(12345, \"David Barnes\")");
+
+            valleyNumToName.Put(12345, "James Kirk");
+            valleyNumToName.Put(45678, "Jean-Luc Picard");
+            valleyNumToName.Put(23456, "Benjamin Sisko");
+            valleyNumToName.Put(34567, "Kathryn Janeway");
+            valleyNumToName.Put(56789, "Jonathan Archer");
+            valleyNumToName.Put(67890, "Jason Isaacs");
+            valleyNumToName.Put(78901, "Carol Freeman");
+
+            Console.WriteLine("The created hash table");
+            Console.WriteLine(valleyNumToName.ToString());
+            Console.WriteLine();
+            Console.WriteLine(valleyNumToName.ShowArray());
+            Console.WriteLine("******************************************");
+
+            Console.WriteLine("Get a single value out from the hash table");
+            Console.WriteLine("valleyNumToName.Get(45678)");
+            Console.WriteLine(valleyNumToName.Get(45678));
+            Console.WriteLine();
+
+            Console.WriteLine("What if we add 2 entries that collide");
+            Console.WriteLine("valleyNumToName.Put(26189, \"First Entry\")");
+            valleyNumToName.Put(26189, "First Entry");
+            Console.WriteLine("valleyNumToName.Put(26092, \"Second Entry\")");
+            valleyNumToName.Put(26092, "Second Entry");
+
+            Console.WriteLine(valleyNumToName.ToString());
+            Console.WriteLine(valleyNumToName.ShowArray());
+
+            Console.WriteLine("Get the first entry out");
+            Console.WriteLine("valleyNumToName.Get(26189)");
+            Console.WriteLine(valleyNumToName.Get(26189));
+            Console.WriteLine("Should get second entry instead. First was overwritten.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //***************************************************************************
+
+            GenericHashTables<string, string> stringOne = new GenericHashTables<string, string>();
+
+            stringOne.Put("My neat key", "Joe Smith");
+            stringOne.Put("other Key", "Freddy Rico");
+
+            Console.WriteLine("Fetch other Key out");
+            Console.WriteLine(stringOne.Get("other Key"));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //***************************************************************************
+
+            Dictionary<int, string> myDict = new Dictionary<int, string>();
+            Console.WriteLine("Use the built in Dictionary for the collision entries");
+            myDict[26189] = "First Entry";
+            myDict[26092] = "Second Entry";
+            Console.WriteLine(myDict[26189]);
+            Console.WriteLine(myDict[26092]);
+            Console.WriteLine("They are both there.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //***************************************************************************
+
+            Dictionary<string, string> stringDict = new Dictionary<string, string>();
+            Console.WriteLine("Use built in Dictionary for a string dict");
+            stringDict["foobar"] = "Joe Smith";
+            stringDict["barbaz"] = "Freddy Rico";
+            Console.WriteLine(stringDict["foobar"]);
+            Console.WriteLine(stringDict["barbaz"]);
+            Console.WriteLine("String keys work tool!");
+            Console.WriteLine();
+            Console.WriteLine();
+
         }
+
     }
+
 }
